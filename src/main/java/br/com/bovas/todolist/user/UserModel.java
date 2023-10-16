5aae387c5e4bab049a8063f0d0e0d2ab4deb49fe
Tiago <tiagaoalb@gmail.com>
2023-10-16 10:30:21 -0300

    chore: update classes

--- src/main/java/br/com/bovas/todolist/user/UserModel.java
+++ src/main/java/br/com/bovas/todolist/user/UserModel.java
@@ -1,8 +1,28 @@
 package br.com.bovas.todolist.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name="tb_users")
 public class UserModel {
 
-    public String username;
-    public String name;
-    public String password;
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID uuid;
+
+    @Column(unique = true)
+    private String username;
+    private String name;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
