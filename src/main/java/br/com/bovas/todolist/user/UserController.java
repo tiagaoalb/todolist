870336bf756380720f2b1be896f224f439458215
Tiago <tiagaoalb@gmail.com>
2023-10-16 18:48:52 -0300

    chore: update UserController

--- src/main/java/br/com/bovas/todolist/user/UserController.java
+++ src/main/java/br/com/bovas/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.bovas.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,10 @@ public class UserController {
         if (user != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists!");
         }
+
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
 
         var userCreated = userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
